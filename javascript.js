dc1204b27339d0ea6cf7e8d1e7094076c2a1f6bc
Ahmad <nazzala8@gmail.com>
2023-06-20 21:17:57 +0300

    finish project

---
+++ javascript.js
@@ -0,0 +1,54 @@
+let sounds = [];
+function addItem(link, sound) {
+  sounds.push(sound);
+  let inputt = document.getElementById("imgs-input");
+  const node = document.createElement("img");
+  node.src = link;
+  node.classList.add("img");
+
+  inputt.appendChild(node);
+  var audio = new Audio(sound);
+  audio.play();
+}
+
+function deleteAll() {
+  document.getElementById("imgs-input").innerHTML = "";
+  console.log(sounds);
+  sounds = [];
+  console.log(sounds);
+}
+
+function deleteItem() {
+  let inputt = document.getElementById("imgs-input");
+  inputt.removeChild(inputt.lastElementChild);
+  sounds.pop();
+}
+function sleep(milliseconds) {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
+async function speak() {
+  for (i of sounds) {
+    var audio = new Audio(i);
+    audio.play();
+    await sleep(1100);
+  }
+}
+
+async function soundOn(sound, goTo) {
+  var audio = new Audio(sound);
+  audio.play();
+  if (
+    goTo.includes("Daily") ||
+    goTo.includes("trees") ||
+    goTo.includes("farming") ||
+    goTo.includes("sub") ||
+    goTo.includes("verb") ||
+    goTo.includes("obj") ||
+    goTo.includes("cor") ||
+    goTo.includes("sen")
+  )
+    await sleep(2000);
+  else await sleep(1000);
+
+  window.location.href = goTo;
+}
